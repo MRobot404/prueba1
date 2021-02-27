6c5d61dddb4e3d0c245db6be3656ff0fb9dd6d56
MRobot404 <76190109+MRobot404@users.noreply.github.com>
2021-02-26 21:47:31 -0600

    Update Main.java

--- src/main/java/com/mycompany/gitprueba/Main.java
+++ src/main/java/com/mycompany/gitprueba/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.gitprueba;
 
 /**
@@ -16,6 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello Friend");
+        System.out.println("thats lame");
     }
     
 }
